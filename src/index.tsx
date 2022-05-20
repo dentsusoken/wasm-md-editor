2b7bd45ed18c35ecd29a5645116e90ea393dc1b8
ShintaroOba <oba.shintaro@isid.co.jp>
2022-05-20 23:31:37 +0900

    Wasm source

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import TextArea from "./components/Textarea";
 import "./index.css";
+import { wasm_main } from "wasm-md-editor";
+// import { wasm_main } from "wasm-md-editor";
 
-const App: React.FC = (): JSX.Element => {
-  return (<>
-    <TextArea
-      autoFocus={false}
-      disabled={false}
-      defaultValue={"# default value"}
-    />
-    <TextArea
-      autoFocus={false}
-      disabled={false}
-      defaultValue={"# defdault value"}
-    />
+export type IWasmMain = {
+  wasmMain: typeof wasm_main;
+};
+
+export type AppType = {
+  text?: string;
+};
+const useMarkdownParser = () => {
+  const [state, setState] = useState<IWasmMain | null>(null);
+  useEffect(() => {
+    (async () => {
+      const wasmContainer = await import("wasm-md-editor");
+      setState(wasmContainer);
+    })();
+  }, []);
+  return state;
+};
+const App: React.FC<AppType> = (text): JSX.Element => {
+  const instance = useMarkdownParser();
+  return (
+    <>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: instance?.wasmMain(text) ?? "",
+        }}
+      />
+      <TextArea
+        autoFocus={false}
+        disabled={false}
+        defaultValue={"# default value"}
+      />
+      <TextArea
+        autoFocus={false}
+        disabled={false}
+        defaultValue={"# defdault value"}
+      />
     </>
   );
 };
