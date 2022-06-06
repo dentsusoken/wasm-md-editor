7bbc6299f4b182c904ad1b3210f44e97cd5b959f
ShintaroOba <oba.shintaro@isid.co.jp>
2022-06-07 00:24:48 +0900

    不要メソッド削除

--- src/components/Text.rs
+++ src/components/Text.rs
@@ -21,19 +21,8 @@ pub fn text() -> Html {
         })
     };
 
-    // let Props { input, on_change } = props.clone();
-    // let oninput = Callback::from(move |input_event: InputEvent| {
-    //     on_change.emit(get_value_from_input_event(input_event))
-    // });
     html! {
         <textarea value={value.to_string()} oninput={on_input} />
     }
 }
 
-fn get_value_from_input_event(e: InputEvent) -> String {
-    let event: Event = e.dyn_into().unwrap_throw();
-    let event_target = event.target().unwrap_throw();
-    let target: HtmlInputElement = event_target.dyn_into().unwrap_throw();
-    web_sys::console::log_1(&target.value().into());
-    target.value()
-}
