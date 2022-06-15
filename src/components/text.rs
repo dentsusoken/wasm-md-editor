1cd41ff98f9d0fed199ba3e52189fcd2a0099b5e
ShintaroOba <oba.shintaro@isid.co.jp>
2022-06-15 22:45:32 +0900

    とちゅこみっと

--- src/components/text.rs
+++ src/components/text.rs
@@ -1,10 +1,10 @@
 use super::markdown::Markdown;
+use pulldown_cmark::{html, Options, Parser};
 use web_sys::HtmlTextAreaElement;
 use yew::prelude::*;
 
 #[function_component(Text)]
 pub fn text() -> Html {
-  
     let value = use_state(|| String::from(""));
     let on_input = {
         let value = value.clone();
@@ -21,11 +21,29 @@ pub fn text() -> Html {
             <div class="item">
                 <textarea rows="140" cols="100" value={value.to_string()} oninput={on_input} />
             </div>
-            <div class="item">
-                <Markdown markdwon_data={value.to_string()} />
-                {value.to_string()}
+            <div class="item" setInnerHTML={cmark(&value)}>
+                //<Markdown markdwon_data={value.to_string()} />
+                // {cmark(&value)}
             </div>
         </div>
         </>
     }
+}
+
+fn cmark(text: &String) -> String {
+    let mut options = Options::empty();
+    options.insert(
+        Options::ENABLE_TABLES
+            | Options::ENABLE_FOOTNOTES
+            | Options::ENABLE_STRIKETHROUGH
+            | Options::ENABLE_TASKLISTS
+            | Options::ENABLE_SMART_PUNCTUATION,
+    );
+    let parser = Parser::new_ext(text, options);
+
+    let mut html_output = String::new();
+    // parser changes rendered String for markdown
+    html::push_html(&mut html_output, parser);
+
+    html_output
 }
